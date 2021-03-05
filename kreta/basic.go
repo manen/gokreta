ef5d5e43b9bffca430af8e2d6ff068a2c19d2de7
manen <varga.bercel.i@gmail.com>
2021-03-05 14:43:55 +0100

    Basic API skeleton

---
+++ kreta/basic.go
@@ -0,0 +1,25 @@
+package kreta
+
+func (s *Session) Grades() {}
+
+func (s *Session) Absences() {}
+
+func (s *Session) Timetable() {}
+
+func (s *Session) Notes() {}
+
+func (s *Session) Notices() {}
+
+func (s *Session) Tests() {}
+
+func (s *Session) Groups() {}
+
+func (s *Session) UserData() {}
+
+func (s *Session) YearOrder() {}
+
+func (s *Session) Homework() {}
+
+func (s *Session) HomeworkComments(id string) {}
+
+func (s *Session) HomeworkComment(id, comment string) {}
